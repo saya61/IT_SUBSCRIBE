9ffa7a2ed3e59bf03208d8f10c8e72cdb3f00a0c
saya61 <msaki061@gmail.com>
2024-06-24 15:17:39 +0900

    Initial commit

---
+++ src/main/java/com/sw/journal/journalcrawlerpublisher/JournalCrawlerPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.sw.journal.journalcrawlerpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JournalCrawlerPublisherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JournalCrawlerPublisherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sw/journal/journalcrawlerpublisher/JournalCrawlerPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.journal.journalcrawlerpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalCrawlerPublisherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
