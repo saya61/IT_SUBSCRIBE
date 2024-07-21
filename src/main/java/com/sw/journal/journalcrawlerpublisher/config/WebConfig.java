d55a6cad597dd286b8df432e6e6fcd4620178cc8
msaki <msaki061@gmail.com>
2024-07-21 14:13:04 +0900

    배포하기 위한 설정 1

--- src/main/java/com/sw/journal/journalcrawlerpublisher/config/WebConfig.java
+++ src/main/java/com/sw/journal/journalcrawlerpublisher/config/WebConfig.java
@@ -13,8 +13,8 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // React 앱의 URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins("http://itscribe.site", "http://www.itscribe.site") // React 앱의 URL
+                        .allowedMethods("*")
                         .allowedHeaders("*") // 모든 헤더를 허용
                         .allowCredentials(true); // 쿠키 및 인증 정보를 허용
             }
