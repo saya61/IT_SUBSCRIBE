2670a530836553d72edc17f53602d6e60a13d4d4
msaki <msaki061@gmail.com>
2024-07-21 12:26:43 +0900

    배포하기 위한 설정 테스트

--- src/test/java/com/sw/journal/journalcrawlerpublisher/service/MemberServiceTest.java
+++ src/test/java/com/sw/journal/journalcrawlerpublisher/service/MemberServiceTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+//@TestPropertySource(locations = "classpath:application-test.properties")
 class MemberServiceTest {
 
     @Autowired
